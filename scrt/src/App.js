0e9fa25fc03ef16d53bab59467631fdef614217f
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2021-10-27 09:55:49 -0700

    Setup Firebase hosting.

--- scrt/src/App.js
+++ scrt/src/App.js
@@ -1,5 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ0MCzup4SKS8PQrvjBnVjMgi9p2EEBVo",
+  authDomain: "scrt-54592.firebaseapp.com",
+  projectId: "scrt-54592",
+  storageBucket: "scrt-54592.appspot.com",
+  messagingSenderId: "76850391699",
+  appId: "1:76850391699:web:a561eccd666b261c46fb66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
