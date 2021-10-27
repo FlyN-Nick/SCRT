0ac67d23cc099032a5d51179dac1e8d4454ae32d
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2021-10-27 10:43:46 -0700

    Indexing.

--- scrt/src/App.js
+++ scrt/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, get } from "firebase/database";
-import { Crypt, RSA } from 'hybrid-crypto-js';
+import { Crypt, RSA } from "hybrid-crypto-js";
+import { nanoid } from "nanoid";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,7 +14,7 @@ const firebaseConfig = {
   projectId: "scrt-54592",
   storageBucket: "scrt-54592.appspot.com",
   messagingSenderId: "76850391699",
-  appId: "1:76850391699:web:a561eccd666b261c46fb66"
+  appId: "1:76850391699:web:a561eccd666b261c46fb66",
 };
 
 // Initialize Firebase
@@ -25,59 +26,58 @@ let publicKey = "";
 let privateKey = "";
 
 // Generate RSA key pair, default key size is 4096 bit
-rsa.generateKeyPair(function(keyPair) {
-    // Callback function receives new key pair as a first argument
-    publicKey = keyPair.publicKey;
+rsa.generateKeyPair(function (keyPair) {
+  // Callback function receives new key pair as a first argument
+  publicKey = keyPair.publicKey;
 });
-rsa.generateKeyPair(function(keyPair) {
+rsa.generateKeyPair(function (keyPair) {
   // Callback function receives new key pair as a first argument
   privateKey = keyPair.privateKey;
 });
 
+// https://stackoverflow.com/questions/8207655/get-time-of-specific-timezone
+function calcTime() {
+  let d = new Date();
+  let utc = d.getTime() + d.getTimezoneOffset() * 60000;
+  let nd = new Date(utc);
+  return nd.toLocaleString();
+}
 
-function writeMessage(msg, msgID) {
+function writeMessage(msg) {
   //console.log(publicKey);
   //console.log(privateKey);
   let encrypted = crypt.encrypt(publicKey, msg);
-  set(ref(db, 'messages/' + msgID), {
+  set(ref(db, "messages/" + calcTime() + nanoid()), {
     message: encrypted,
   });
 }
 
 function App() {
-    const [message, setMessage] = React.useState("");
+  const [message, setMessage] = React.useState("");
 
-    let handleSubmit = (e) => {
-	e.preventDefault()
-	console.log(message)
-	setMessage("")
-	//writeMessage(message, '1');
-    }
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(message);
+    writeMessage(message);
+    setMessage("");
+  };
 
-    //writeMessage('Hello World', '1');
-
-    return (
-	<div className="App">
-	    <header className="App-header">
-		<div
-		    onClick = {() => {
-
-		    }}
-		>
-		    send a message
-		</div>
-		<form onSubmit={handleSubmit} >
-		    <input className="p-3 mt-3"
-			onChange={(e) => {
-			    setMessage(e.target.value)
-			}}
-			value={message}
-		    >
-		    </input>
-		</form>
-	    </header>
-	</div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div onClick={() => {}}>send a message</div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="p-3 mt-3"
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            value={message}
+          ></input>
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
