a07a8e5aae6e1232e0f5b6137d2716917ab5010c
Huxley Marvit <huxmarv@nuevaschool.org>
2021-10-27 10:09:25 -0700

    writing messages!

--- scrt/src/App.js
+++ scrt/src/App.js
@@ -29,18 +29,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Yoooooo.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	  <div onClick = {() => {
+	      writeMessage("heya", 12)
+	      console.log("messaging")
+	  }}
+	  > send a message</div>
       </header>
     </div>
   );
