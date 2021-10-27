90293c4cbc8abca9edf3bf21bf1672754b6c1abe
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2021-10-27 10:04:14 -0700

    Setting up realtime db.

--- scrt/src/App.js
+++ scrt/src/App.js
@@ -2,8 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, set, get } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,8 +16,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+function writeMessage(msg, msgID) {
+  set(ref(db, 'messages/' + msgID), {
+    message: msg,
+  });
+}
 
 function App() {
+  writeMessage('Hello World', '1');
   return (
     <div className="App">
       <header className="App-header">
